6316a1c0009f5391f0d09b75072b7b5bbb6bc00f
BaekJiwan <bn1357nb@gmail.com>
2022-03-06 15:14:50 +0900

    java_algorithm_stack_practice

--- ch06_stack/src/MyStack.java
+++ ch06_stack/src/MyStack.java
@@ -16,10 +16,14 @@ public class MyStack<T> {
 
     public static void main(String[] args) {
         MyStack<Integer> ms = new MyStack<Integer>();
-        ms.push(1);
-        ms.push(2);
-        System.out.println(ms.pop());
         ms.push(3);
+        ms.push(4);
+        ms.push(2);
+        ms.push(6);
+        System.out.println(ms.pop());
+        ms.push(7);
+        System.out.println(ms.pop());
+        System.out.println(ms.pop());
         System.out.println(ms.pop());
         System.out.println(ms.pop());
     }
