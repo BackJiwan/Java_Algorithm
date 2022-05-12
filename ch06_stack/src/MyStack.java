2128ddacc7d4808a9e643b20e0ce9a58fee2690a
BaekJiwan <bn1357nb@gmail.com>
2022-05-12 20:17:51 +0900

    java_algorithm_stack_practice

--- ch06_stack/src/MyStack.java
+++ ch06_stack/src/MyStack.java
@@ -2,5 +2,13 @@ import java.util.Stack;
 import java.util.ArrayList;
 
 public class MyStack<T> {
-    private
+    private ArrayList<T> stack = new ArrayList<T>();
+
+    public void push(T item){
+        stack.add(item);
+    }
+
+    public T pop(){
+
+    }
 }
