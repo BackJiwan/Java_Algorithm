a65614888e066207cdc6c3c93543e0f8404c4b28
BaekJiwan <bn1357nb@gmail.com>
2022-06-17 00:13:46 +0900

    java_algorithm_stack_practice

--- ch06_stack/src/MyStack.java
+++ ch06_stack/src/MyStack.java
@@ -37,8 +37,5 @@ public class MyStack<T>{
         System.out.println(ms.pop());
         ms.push(3);
         System.out.println(ms.pop());
-        ms.push(3);
-        System.out.println(ms.pop());
-
     }
 }
