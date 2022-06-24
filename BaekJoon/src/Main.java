f0535bdd6e221c591ff9a5b258ad7216071dc62d
BaekJiwan <bn1357nb@gmail.com>
2022-06-24 22:16:18 +0900

    Practice algorithm

--- BaekJoon/src/Main.java
+++ BaekJoon/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[] args){
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+    public static void main(String[]args){
+        double i,j;
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        j = sc.nextInt();
+        System.out.println(i+j);
+        System.out.println(i-j);
+        System.out.println(i*j);
+        System.out.println(i/j);
+        System.out.println(i%j);
     }
 }
