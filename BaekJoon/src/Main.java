2f8a20306c5ba82ee098477db755e7c6670f9df9
BaekJiwan <bn1357nb@gmail.com>
2022-06-27 00:39:35 +0900

    Practice algorithm

--- BaekJoon/src/Main.java
+++ BaekJoon/src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
-
 public class Main{
     public static void main(String[]args) {
-        int a,b,cnt;
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
 
-        cnt = sc.nextInt();
-        for(int i = 0;i<cnt;i++){
-            a = sc.nextInt();
-            b = sc.nextInt();
-            System.out.println(a+b);
-        }
+       int n,a,b;
+       n = sc.nextInt();
+       String[] s = new String[n];
+
+       for(int i=1;i<=n;i++){
+           a=sc.nextInt();
+           b=sc.nextInt();
+           s[i-1] = String.format("Case #%d: %d",i,a+b);
+       }
+       for(int i=0;i<n;i++){
+           System.out.println(s[i]);
+       }
     }
 }
+
