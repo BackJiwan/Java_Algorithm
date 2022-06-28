2a4d4a39203dc3618c74e88006e6c9836b45da9f
BaekJiwan <bn1357nb@gmail.com>
2022-06-28 17:11:57 +0900

    Practice algorithm

--- BaekJoon/src/bj10818.java
+++ BaekJoon/src/bj10818.java
@@ -3,21 +3,20 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class bj10818 {
-    public static void Main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         ArrayList<Integer> list1 = new ArrayList<Integer>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st ;
-        int size = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine()," ");
+        int size = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         br.close();
 
         for(int i=0;i<size;i++){
-            list1.set(i, Integer.parseInt(st.nextToken()));
-        }
+            list1.add(Integer.parseInt(st.nextToken()));
+        } //입력값을 list1에 순차대입
 
         int min = list1.get(0);
-        int max = list1.get(0);
+        int max = list1.get(0); //최대최솟값을 배열의 최초값으로 초기화
 
         for(int j=1;j<size;j++) {
             if (list1.get(j) < min) {
@@ -29,5 +28,6 @@ public class bj10818 {
             } else
                 continue;
         }
+        System.out.print(min+" "+max);
     }
 }
