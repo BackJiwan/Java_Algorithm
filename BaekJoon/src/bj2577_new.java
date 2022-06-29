37644274650d837c05608169049d96413770dc5b
BaekJiwan <bn1357nb@gmail.com>
2022-06-29 23:34:21 +0900

    Practice algorithm

--- BaekJoon/src/bj2577_new.java
+++ BaekJoon/src/bj2577_new.java
@@ -1,2 +1,17 @@
+import java.io.*;
+
 public class bj2577_new {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int val = ((Integer.parseInt(br.readLine()))*(Integer.parseInt(br.readLine()))*(Integer.parseInt(br.readLine())));
+        int[] arr= new int[10];
+        String str = String.valueOf(val);
+
+        for(int i=0;i<str.length();i++){
+            arr[(str.charAt(i)-48)]++;
+        }
+        for(int j:arr)
+            System.out.println(j);
+    }
 }
