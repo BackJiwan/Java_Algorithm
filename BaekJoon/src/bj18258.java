0080af0b17fc93b4fc8c2c45aa8e27649d2e7eec
BaekJiwan <bn1357nb@gmail.com>
2022-07-04 17:37:47 +0900

    Practice algorithm

--- BaekJoon/src/bj18258.java
+++ BaekJoon/src/bj18258.java
@@ -1,12 +1,11 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class bj18258 {
+    ArrayList<Integer> mq = new ArrayList<>();
     public static void main(String[] args){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    }
-    public class Queue{
-        public void push(int item){
 
-        }
     }
+
 }
