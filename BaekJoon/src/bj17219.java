d6aaf6d87ff68796bb34c4b95f8c1988cbae91fa
BaekJiwan <bn1357nb@gmail.com>
2022-07-25 21:08:01 +0900

    Practice

--- BaekJoon/src/bj17219.java
+++ BaekJoon/src/bj17219.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+
 public class bj17219 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
