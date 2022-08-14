1eda748a3008b401cd6e9f345bf9ff6666295e0c
BaekJiwan <bn1357nb@gmail.com>
2022-08-14 12:06:14 +0900

    Practice

--- BaekJoon/src/bj11279.java
+++ BaekJoon/src/bj11279.java
@@ -13,79 +13,83 @@ public class bj11279 {
         int n = Integer.parseInt(br.readLine()); //연산의 개수 N
         for(int i=0;i<n;i++){
             int k = Integer.parseInt(br.readLine()); //명령
-            if(k==0){
-                sb.append(heap.pop()).append('\n');
-            } else{
+            if(k>0){
                 heap.insert(k);
+            } else{
+                if(heap.heapArray.size() == 1){
+                    sb.append('0').append('\n');
+                }else {
+                    sb.append(heap.pop()).append('\n');
+                }
             }
         }
         System.out.println(sb.toString());
     }
+    static class maxHeap{
+        public ArrayList<Integer> heapArray = null;
 
-}
-class maxHeap{
-    public ArrayList<Integer> heapArray = null;
-
-    public maxHeap(Integer data){
-        this.heapArray = new ArrayList<Integer>();
-        this.heapArray.add(null);
-        this.heapArray.add(data);
-    }
-    public boolean move_up(Integer inserted_idx){
-        if(inserted_idx <= 1){
-            return false;
+        public maxHeap(Integer data){
+            heapArray = new ArrayList<Integer>();
+            heapArray.add(null);
+            heapArray.add(data);
         }
-        Integer parent_idx = inserted_idx / 2;
-        if(this.heapArray.get(parent_idx) < this.heapArray.get(inserted_idx)){
-            return true;
-        } else{
-            return false;
-        }
-    }
-    public boolean insert(Integer data){
-        Integer inserted_idx,parent_idx;
-
-        if(heapArray == null){
-            this.heapArray = new ArrayList<Integer>();
-            this.heapArray.add(null);
-            this.heapArray.add(data);
-            return true;
-        } else{
-            this.heapArray.add(data);
-            inserted_idx = this.heapArray.size() - 1;
-
-            while(this.move_up(inserted_idx)){
-                parent_idx = inserted_idx /2;
-                Collections.swap(this.heapArray,inserted_idx,parent_idx);
-                inserted_idx = parent_idx;
-            }
-            return true;
-        }
-    }
-    public boolean move_down(Integer popped_idx) {
-        Integer left_child_popped_idx, right_child_popped_idx;
-
-        left_child_popped_idx = popped_idx * 2;
-        right_child_popped_idx = popped_idx * 2 + 1;
-
-        //1: 왼쪽 자식 노드도 없을 경우
-        if (left_child_popped_idx >= this.heapArray.size()) {
-            return false;
-        } else if (right_child_popped_idx >= this.heapArray.size()) { //오른쪽 자식만 없는 경우
-            if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
-                return true;
-            } else {
+        public boolean move_up(Integer inserted_idx){
+            if(inserted_idx <= 1){
                 return false;
             }
-        } else { //왼쪽 오른쪽 자식 모두 있을때
-            if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
-                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
+            Integer parent_idx = inserted_idx / 2;
+            if(heapArray.get(parent_idx) < heapArray.get(inserted_idx)){
+                return true;
+            } else{
+                return false;
+            }
+        }
+        public boolean insert(Integer data){
+            Integer inserted_idx,parent_idx;
+
+            if(heapArray == null){
+                heapArray = new ArrayList<Integer>();
+                heapArray.add(null);
+                heapArray.add(data);
+                return true;
+            } else{
+                heapArray.add(data);
+                inserted_idx = heapArray.size() - 1;
+
+                while(move_up(inserted_idx)){
+                    parent_idx = inserted_idx /2;
+                    Collections.swap(heapArray,inserted_idx,parent_idx);
+                    inserted_idx = parent_idx;
+                    if(inserted_idx == 1)
+                        break;
+                }
+                return true;
+            }
+        }
+        public boolean move_down(Integer popped_idx) {
+            Integer left_child_popped_idx, right_child_popped_idx;
+
+            left_child_popped_idx = popped_idx * 2;
+            right_child_popped_idx = popped_idx * 2 + 1;
+
+            //1: 왼쪽 자식 노드도 없을 경우
+            if (left_child_popped_idx >= heapArray.size()) {
+                return false;
+            } else if (right_child_popped_idx >= heapArray.size()) { //오른쪽 자식만 없는 경우
+                if (heapArray.get(popped_idx) < heapArray.get(left_child_popped_idx)) {
                     return true;
                 } else {
                     return false;
                 }
-            } else{
-                    if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
+            } else { //왼쪽 오른쪽 자식 모두 있을때
+                if (heapArray.get(left_child_popped_idx) > heapArray.get(right_child_popped_idx)) {
+                    if (heapArray.get(popped_idx) < heapArray.get(left_child_popped_idx)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else{
+                    if (heapArray.get(popped_idx) < heapArray.get(right_child_popped_idx)) {
                         return true;
                     } else {
                         return false;
@@ -94,40 +98,42 @@ class maxHeap{
             }
         }
 
-    public Integer pop(){
-        Integer returned_data,popped_idx,left_child_popped_idx, right_child_popped_idx;
+        public Integer pop(){
+            Integer returned_data,popped_idx,left_child_popped_idx, right_child_popped_idx;
 
-        if(this.heapArray.size() <= 1){
-            return 0;
-        }
-        returned_data = this.heapArray.get(1); //루트노드를 저장
-        this.heapArray.set(1,this.heapArray.get(this.heapArray.size()-1)); //마지막 노드를 루트로 올려주기
-        this.heapArray.remove(this.heapArray.size()-1); // 마지막 노드를 삭제
-        popped_idx = 1;
+            if(heapArray.size() <= 1){
+                return 0;
+            }
+            returned_data = heapArray.get(1); //루트노드를 저장
+            heapArray.set(1,heapArray.get(heapArray.size()-1)); //마지막 노드를 루트로 올려주기
+            heapArray.remove(heapArray.size()-1); // 마지막 노드를 삭제
+            popped_idx = 1;
 
-        while (this.move_down(popped_idx)) {
-            left_child_popped_idx = popped_idx * 2;
-            right_child_popped_idx = popped_idx * 2 + 1;
+            while (move_down(popped_idx)) {
+                left_child_popped_idx = popped_idx * 2;
+                right_child_popped_idx = popped_idx * 2 + 1;
 
-            if (right_child_popped_idx >= this.heapArray.size()) { //오른쪽 자식만 없는 경우
-                if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
-                    Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
-                    popped_idx = left_child_popped_idx;
-                }
-            } else { //왼쪽 오른쪽 자식 모두 있을때
-                if (this.heapArray.get(left_child_popped_idx) > this.heapArray.get(right_child_popped_idx)) {
-                    if (this.heapArray.get(popped_idx) < this.heapArray.get(left_child_popped_idx)) {
-                        Collections.swap(this.heapArray, popped_idx, left_child_popped_idx);
+                if (right_child_popped_idx >=heapArray.size()) { //오른쪽 자식만 없는 경우
+                    if (heapArray.get(popped_idx) < heapArray.get(left_child_popped_idx)) {
+                        Collections.swap(heapArray, popped_idx, left_child_popped_idx);
                         popped_idx = left_child_popped_idx;
-                    }  else{
-                        if (this.heapArray.get(popped_idx) < this.heapArray.get(right_child_popped_idx)) {
-                            Collections.swap(this.heapArray, popped_idx, right_child_popped_idx);
-                            popped_idx = right_child_popped_idx;
+                    }
+                } else { //왼쪽 오른쪽 자식 모두 있을때
+                    if (heapArray.get(left_child_popped_idx) > heapArray.get(right_child_popped_idx)) {
+                        if (heapArray.get(popped_idx) < heapArray.get(left_child_popped_idx)) {
+                            Collections.swap(heapArray, popped_idx, left_child_popped_idx);
+                            popped_idx = left_child_popped_idx;
+                        }  else{
+                            if (heapArray.get(popped_idx) < heapArray.get(right_child_popped_idx)) {
+                                Collections.swap(heapArray, popped_idx, right_child_popped_idx);
+                                popped_idx = right_child_popped_idx;
+                            }
                         }
                     }
                 }
             }
+            return returned_data;
         }
-        return returned_data;
     }
 }
+
