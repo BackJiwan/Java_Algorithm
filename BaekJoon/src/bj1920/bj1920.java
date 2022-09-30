f87e64fb699d763403e08fd028fa1df24ba6aa95
BackJiwan <bn1357nb@gmail.com>
2022-09-30 22:53:30 +0900

    Practice

--- BaekJoon/src/bj1920/bj1920.java
+++ BaekJoon/src/bj1920/bj1920.java
@@ -1,4 +1,15 @@
 package bj1920;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class bj1920 {
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
 }
